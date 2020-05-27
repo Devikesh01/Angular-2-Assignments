2062922a31adfdb524caac36dcf11e042e3656fd
Devikesh Shende <61633788+Devikesh01@users.noreply.github.com>
2020-05-27 15:02:36 +0530

    Add files via upload

---
+++ Assignment 5/Assignment 5_all/app/firstcomponent/firstcomponent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-firstcomponent',
+  templateUrl: './firstcomponent.component.html',
+
+})
+export class FirstcomponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ Assignment 5/Assignment 5_all/app/secondcomponent/secondcomponent.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-secondcomponent',
+  templateUrl: './secondcomponent.component.html',
+
+})
+export class SecondcomponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
